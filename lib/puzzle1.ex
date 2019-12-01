ef4503b6e584927965d73726c5a82787d3533ca9
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-12-01 17:05:55 -0300

    add @specs

--- lib/puzzle1.ex
+++ lib/puzzle1.ex
@@ -3,11 +3,14 @@ defmodule Puzzle1 do
   First puzzle. Calculate fuel required for module masses.
   """
 
+  @type mass :: non_neg_integer()
+
   @doc """
   Reads a file with one integer per line representing each module mass
 
   Crashes on invalid input
   """
+  @spec from_input(Path.t()) :: mass()
   def from_input(path) do
     path
     |> File.stream!()
@@ -20,6 +23,7 @@ defmodule Puzzle1 do
   Receives a list of module masses and computes the total required fuel for
   those modules and the fuel required for those modules.
   """
+  @spec total_fuel([mass()]) :: mass()
   def total_fuel(modules) do
     Enum.reduce(modules, 0, fn module, acc ->
       acc + fuel_for_module(module)
@@ -30,6 +34,7 @@ defmodule Puzzle1 do
   Computes the amount of fuel for a given module.
   Will iterate to compute fuel required for fuel.
   """
+  @spec fuel_for_module(mass()) :: mass()
   def fuel_for_module(module) do
     module
     |> Stream.unfold(fn
@@ -44,6 +49,7 @@ defmodule Puzzle1 do
   end
 
   @doc "Computes the amount of fuel for the given mass."
+  @spec fuel(mass()) :: mass()
   def fuel(mass) do
     mass
     |> Kernel./(3)
