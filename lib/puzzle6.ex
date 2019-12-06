7bf3bcac033f0fba90e6d640e347afc4003b450d
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2019-12-06 17:09:39 -0300

    Day 6, second star

--- lib/puzzle6.ex
+++ lib/puzzle6.ex
@@ -4,6 +4,35 @@ defmodule Puzzle6 do
   """
 
   @root "COM"
+  @santa "SAN"
+  @you "YOU"
+
+  def from_you_to_san(file \\ "test/support/puzzle6/input.txt") do
+    nodes = from_input(file)
+
+    nodes_to_root = fn node ->
+      node
+      |> to_root(nodes, [])
+      |> Enum.drop(-1)
+    end
+
+    nodes_to_root.(@you)
+    |> nodes_between(nodes_to_root.(@santa))
+    |> length()
+    |> Kernel.-(1)
+  end
+
+  defp nodes_between([@root | you_tail], [@root | santa_tail]) do
+    nodes_between(@root, you_tail, santa_tail)
+  end
+
+  defp nodes_between(_previous_head, [head | you_tail], [head | santa_tail]) do
+    nodes_between(head, you_tail, santa_tail)
+  end
+
+  defp nodes_between(common, to_you, to_santa) do
+    Enum.reverse(to_you) ++ [common] ++ to_santa
+  end
 
   def orbits do
     nodes = from_input("test/support/puzzle6/input.txt")
@@ -13,26 +42,27 @@ defmodule Puzzle6 do
     |> orbits(nodes, 0)
   end
 
-  defp orbits([], _, acc) do
-    acc
-  end
+  defp orbits([], _, acc), do: acc
 
   defp orbits([vertice | tail], node_map, acc) do
-    acc = to_root(vertice, node_map, acc)
+    acc =
+      vertice
+      |> to_root(node_map, [])
+      |> length()
+      |> Kernel.-(1)
+      |> Kernel.+(acc)
+
     orbits(tail, node_map, acc)
   end
 
-  def to_root(@root, _, acc) do
-    acc
-  end
+  def to_root(@root, _, acc), do: [@root | acc]
 
   def to_root(node, node_map, acc) do
     parent = Map.get(node_map, node)
-    to_root(parent, node_map, acc + 1)
+    to_root(parent, node_map, [node | acc])
   end
 
-  @spec from_input(Path.t()) :: term()
-  def from_input(path) do
+  defp from_input(path) do
     path
     |> File.stream!()
     |> Stream.map(&String.trim/1)
@@ -44,7 +74,8 @@ defmodule Puzzle6 do
     Map.put_new(map, child, parent)
   end
 
-  def to_vertice(<<parent::binary-size(3), ")", child::binary-size(3)>>) do
+  def to_vertice(line) do
+    [parent, child] = String.split(line, ")")
     {parent, child}
   end
 end
