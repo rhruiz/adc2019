2a58ff1702981ce9d04082d94dc3c1faeb825aeb
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2019-12-11 19:43:14 -0300

    wip

--- lib/puzzle10.ex
+++ lib/puzzle10.ex
@@ -3,7 +3,9 @@ defmodule Puzzle10 do
   Let's play asteroids
   """
 
-  @spec from_file(Path.t()) :: map()
+  @type asteroids :: map()
+
+  @spec from_file(Path.t()) :: asteroids()
   def from_file(path) do
     path
     |> File.stream!()
@@ -18,7 +20,37 @@ defmodule Puzzle10 do
     |> Enum.into(%{})
   end
 
-  @spec best_asteroid(map()) :: {{integer(), integer()}, non_neg_integer()}
+  @spec vaporize(asteroids(), non_neg_integer() | :infinity) :: non_neg_integer()
+  def vaporize(map, limit) do
+    {position, _} = best_asteroid(map)
+
+    vaporize(map, visibles(map, position), position, limit, nil, 0)
+  end
+
+  defp vaporize(_map, [], _position, _limit, asteroid, count) do
+    {count, asteroid}
+  end
+
+  defp vaporize(_map, _visible, _position, limit, asteroid, count) when count > limit do
+    {limit, asteroid}
+  end
+
+  defp vaporize(map, visibles, position, limit, asteroid, count) do
+    {new_map, new_asteroid, new_count} =
+      visibles
+      |> Enum.sort_by(&elem(&1, 1))
+      |> Enum.reduce({map, asteroid, count}, fn
+        _, acc = {_, _, count} when count > limit ->
+          acc
+
+        {vap, _angle}, {map, _asteroid, count} ->
+          {Map.put(map, vap, "x"), vap, count + 1}
+      end)
+
+    vaporize(new_map, visibles(new_map, position), position, limit, new_asteroid, new_count)
+  end
+
+  @spec best_asteroid(asteroids()) :: {{integer(), integer()}, non_neg_integer()}
   def best_asteroid(map) do
     best_asteroid(map, Map.to_list(map), {{0, 0}, 0})
   end
@@ -33,17 +65,38 @@ defmodule Puzzle10 do
 
   defp best_asteroid(map, [{{x, y}, "#"} | tail], current) do
     visibles =
-      for {{x2, y2}, "#"} <- Map.to_list(map), {x, y} != {x2, y2} do
-        dx = x2 - x
-        dy = y2 - y
-
-        :math.atan2(dy, dx)
-      end
+      map
+      |> visibles(x, y)
       |> Enum.uniq()
       |> Enum.count()
 
     max = Enum.max_by([current, {{x, y}, visibles}], &elem(&1, 1))
 
     best_asteroid(map, tail, max)
+  end
+
+  defp visibles(map, {x, y}), do: visibles(map, x, y)
+
+  defp visibles(map, x, y) do
+    distance_from =
+      fn {x2, y2} ->
+        :math.sqrt(:math.pow(x2 - x, 2) + :math.pow(y2 - y, 2))
+      end
+
+    for {{x2, y2}, "#"} <- Map.to_list(map), {x, y} != {x2, y2} do
+      dx = x2 - x
+      dy = y2 - y
+
+      {{x2, y2}, :math.atan2(dy, dx)}
+    end
+    |> Enum.group_by(&elem(&1, 1))
+    |> Map.values()
+    |> Enum.map(fn values ->
+      values
+      |> Enum.sort_by(fn {position, angle} ->
+        [distance_from.(position), angle]
+      end)
+      |> hd()
+    end)
   end
 end
