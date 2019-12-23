bf38fa689cc985ad29be2e77e53ddb71982ffc4d
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-12-23 15:26:57 -0300

    Day 19, second star

--- lib/puzzle19.ex
+++ lib/puzzle19.ex
@@ -4,20 +4,51 @@ defmodule Puzzle19 do
   """
 
   def scan(program) do
-    map =
-      Enum.reduce(0..49, %{}, fn x, map ->
-        Enum.reduce(0..49, map, fn y, map ->
-          tractor = IntcodeRunner.start_link(program)
-          IntcodeRunner.input(tractor, x)
-          IntcodeRunner.input(tractor, y)
-
-          Map.put(map, {x, y}, IntcodeRunner.output(tractor))
-        end)
+    0..49
+    |> Enum.reduce(0, fn x, counter ->
+      Enum.reduce(0..49, counter, fn y, counter ->
+        counter + at(program, x, y)
       end)
-
-    Enum.reduce(map, 0, fn
-      {_position, 1}, counter -> counter + 1
-      _, counter -> counter
     end)
+  end
+
+  defp at(program, x, y) do
+    tractor = IntcodeRunner.start_link(program)
+    IntcodeRunner.input(tractor, x)
+    IntcodeRunner.input(tractor, y)
+
+    IntcodeRunner.output(tractor)
+  end
+
+  defp seq(initial), do: Stream.unfold(initial, fn n -> {n, n + 1} end)
+
+  def find_square(program) do
+    seq(100)
+    |> Stream.transform(0, fn y, lastx ->
+      x1 =
+        lastx
+        |> seq()
+        |> Enum.find(fn x -> at(program, x, y) == 1 end)
+
+      if at(program, x1 + 99, y) == 1 do
+        x2 =
+          x1
+          |> seq()
+          |> Enum.find(fn x -> at(program, x + 99, y) == 0 end)
+          |> Kernel.-(1)
+
+        Enum.find(x1..x2, fn x ->
+          at(program, x, y + 99) == 1 && at(program, x + 99, y + 99) == 1
+        end)
+        |> (fn
+              nil -> {[], x1}
+              x -> {[{x, y}], x1}
+            end).()
+      else
+        {[], x1}
+      end
+    end)
+    |> Stream.take(1)
+    |> Enum.min_by(fn {x, y} -> x + y end)
   end
 end
--- test/puzzle19_test.exs
+++ test/puzzle19_test.exs
@@ -4,9 +4,20 @@ defmodule Puzzle19Test do
   import Puzzle19
 
   describe "scan/1" do
-    assert 164 =
-             "test/support/puzzle19/input.txt"
-             |> Intcode.read_file()
-             |> scan()
+    test "finds in a 50x50 area" do
+      assert 164 =
+               "test/support/puzzle19/input.txt"
+               |> Intcode.read_file()
+               |> scan()
+    end
+  end
+
+  describe "find_square/1" do
+    test "finds where a 100x100 square fits" do
+      assert {1308, 1049} =
+               "test/support/puzzle19/input.txt"
+               |> Intcode.read_file()
+               |> find_square()
+    end
   end
 end
