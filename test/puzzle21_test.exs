ae16f474f826690987ac2a005e342409fbca7720
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-12-23 21:04:30 -0300

    Day 21, second star

--- test/puzzle21_test.exs
+++ test/puzzle21_test.exs
@@ -16,4 +16,20 @@ defmodule Puzzle21Test do
     assert_receive {:halted, ^pid}
     assert_receive {:output, ^pid, 19_354_437}
   end
+
+  test "avoids holes with extended sensors" do
+    target = self()
+
+    pid =
+      "test/support/puzzle21/input.txt"
+      |> Intcode.read_file()
+      |> IntcodeRunner.start_link(puts: fn x -> send(target, {:output, self(), x}) end)
+
+    assembly = 'OR A J\nAND B J\nAND C J\nNOT J J\nAND D J\nOR E T\nOR H T\nAND T J\nRUN\n'
+
+    Enum.each(assembly, fn xhr -> IntcodeRunner.input(pid, xhr) end)
+
+    assert_receive {:halted, ^pid}, :timer.seconds(5)
+    assert_receive {:output, ^pid, 1_145_373_084}
+  end
 end
